59dc1c2442737f509a23a0ad89b6b8a4a43810a7
All-in-Miner <39795801+All-in-Miner@users.noreply.github.com>
2018-06-25 14:47:36 +0200

    Adding Claymore with OhGodABiosTweak
    Adding Claymore with OhGodABiosTweak
    using the Powershell Wrapper
    hence output is displayed only at the end of each interval!
    Feel free to improve this!

--- Miners/EthashClaymore-11.0.ps1
+++ Miners/EthashClaymore-11.0.ps1
@@ -1,7 +1,8 @@
 . .\Include.ps1
 
-$Path = ".\\Bin\\Ethash-Claymore\\EthDcrMiner64.exe"
-$Uri = "https://github.com/nemosminer/Claymores-Dual-Ethereum/releases/download/v11.7/ClaymoreEthMiner.v11.7.7z"
+$Path = ".\\Bin\\Ethash-Claymore-11.0\\EthDcrMiner64.bat"
+$Uri = "https://github.com/All-in-Miner/Claymore-Dual-Miner/releases/download/v11.0/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal.AMD.NVIDIA.GPU.Miner.v11.0.zip"
+
 $Commands = [PSCustomObject]@{
     "ethash" = " -di $($SelGPUCC.Replace(',',''))" #Ethash(fastest)
 }
@@ -13,10 +14,10 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type = "NVIDIA"
         Path = $Path
         Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.MinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Claymore"
         Port = $Variables.MinerAPITCPPort #3333
-        Wrap = $false
+        Wrap = $true
         URI = $Uri
 		User = $Pools.(Get-Algorithm($_)).User
     }
--- Wrapper.ps1
+++ Wrapper.ps1
@@ -30,35 +30,37 @@ do {
     $PowerShell.Streams.Verbose.ReadAll() | ForEach-Object {
         $Line = $_
 
-        if ($Line -like "*Total*") {
-            $Words = $Line -split " "
-            $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -First 1)) - 1]
+		if ($Command -notlike "*EthDcrMiner64*") {
+			if ($Line -like "*Total*") {
+				$Words = $Line -split " "
+				$HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -First 1)) - 1]
 
-            switch ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -First 1))]) {
-                 "s/s" {$HashRate *= [Math]::Pow(1000, 0)}
-                "ks/s" {$HashRate *= [Math]::Pow(1000, 1)}
-                "ms/s" {$HashRate *= [Math]::Pow(1000, 2)}
-                "gs/s" {$HashRate *= [Math]::Pow(1000, 3)}
-                "ts/s" {$HashRate *= [Math]::Pow(1000, 4)}
-                "ps/s" {$HashRate *= [Math]::Pow(1000, 5)}
-            }
+				switch ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -First 1))]) {
+					 "s/s" {$HashRate *= [Math]::Pow(1000, 0)}
+					"ks/s" {$HashRate *= [Math]::Pow(1000, 1)}
+					"ms/s" {$HashRate *= [Math]::Pow(1000, 2)}
+					"gs/s" {$HashRate *= [Math]::Pow(1000, 3)}
+					"ts/s" {$HashRate *= [Math]::Pow(1000, 4)}
+					"ps/s" {$HashRate *= [Math]::Pow(1000, 5)}
+				}
 
-            $HashRate | Set-Content ".\Bminer.txt"
-        } elseif ($Line -like "*Total*") {
-            $Words = $Line -split " "
-            $HashRate = [Decimal]($Words -like "*/s*" -replace ',', '' -replace "[^0-9.]",'' | Select-Object -First 1)
+				$HashRate | Set-Content ".\Bminer.txt"
+			} elseif ($Line -like "*Total*") {
+				$Words = $Line -split " "
+				$HashRate = [Decimal]($Words -like "*/s*" -replace ',', '' -replace "[^0-9.]",'' | Select-Object -First 1)
 
-            switch ($Words -like "*S/s*" -replace "[0-9.,]",'' | Select-Object -First 1) {
-                 "S/s" {$HashRate *= [Math]::Pow(1000, 0)}
-                "KS/s" {$HashRate *= [Math]::Pow(1000, 1)}
-                "mS/s" {$HashRate *= [Math]::Pow(1000, 2)}
-                "MS/s" {$HashRate *= [Math]::Pow(1000, 2)}
-            }
-			$HashRate = [int]$HashRate
-            $HashRate | Set-Content ".\Bminer.txt"
-        }
-
-        $Line
+				switch ($Words -like "*S/s*" -replace "[0-9.,]",'' | Select-Object -First 1) {
+					 "S/s" {$HashRate *= [Math]::Pow(1000, 0)}
+					"KS/s" {$HashRate *= [Math]::Pow(1000, 1)}
+					"mS/s" {$HashRate *= [Math]::Pow(1000, 2)}
+					"MS/s" {$HashRate *= [Math]::Pow(1000, 2)}
+				}
+				$HashRate = [int]$HashRate
+				$HashRate | Set-Content ".\Bminer.txt"
+			}
+		}
+		$Line
+			
     }
 
     if ((Get-Process | Where-Object Id -EQ $ControllerProcessID) -eq $null) {$PowerShell.Stop() | Out-Null}
